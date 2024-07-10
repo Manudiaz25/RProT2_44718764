3c2c316d81df062f20204e9e77ea2748bd06eb81
Manudiaz25 <josemad2502@gmail.com>
2024-07-09 22:17:52 -0300

    Add files via upload

---
+++ front/headview.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Aston Watches</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css"
+    
+    
+   </head>
+      <header>
+            <h1>bienvenido a aston watches</h1> 
+            <div class="container">
+              <section>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#"><img src="assets/img/tecnologico.webp" width="100" height="100"></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">quienes somos <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">acerca de</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">registrarse</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">login</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+            </section>
+
+
+  </section>
+
+      </header>
+    <body>
+   <script src="assets/js/bootstrap.bundle.min.js"></script>
+   <div id="carouselExampleCaptions" class="carousel slide">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/watches1.jpg.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>aqui puedes encontrar todo tipo de relojes</h5>
+        <p>nuestro alto precio se debe a nuestra extrema calidad</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/tecnologico1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Smartwatch</h5>
+        <p>aqui podemos un modelo tecnologico recomendado para deportistas</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/analogico.webp" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>estilos analogicos</h5>
+        <p>de los mas confiables y mas comprados un exelente clasico</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>s
+</div>
+
+<div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-4">La calidad y el estatus nos define.</h1>
+            <p class="lead">(los siguientes productos son los unicos que se encuentran en el mercado y solo pueden ser adquiridos aqui).</p>
+        </div>
+    </div>
+
+    <
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4 product">
+                <div class="card">
+                    <img src="assets/img/excentrico1.jpg" class="card-img-top" alt="Watch 1">
+                    <div class="card-body">
+                        <h5 class="card-title">Reloj Pulsera azul zafiro</h5>
+                        <p class="card-text">$900</p>
+                        <a href="#" class="btn btn-primary">Compra aqui!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 product">
+                <div class="card">
+                    <img src="assets/img/excentrico2.webp" class="card-img-top" alt="Watch 2">
+                    <div class="card-body">
+                        <h5 class="card-title">Reloj analogico Heliocentric</h5>
+                        <p class="card-text">$75.000.</p>
+                        <a href="#" class="btn btn-primary">Compra aqui!</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 product">
+                <div class="card">
+                    <img src="assets/img/excentrico3.webp" class="card-img-top" alt="Watch 3">
+                    <div class="card-body">
+                        <h5 class="card-title">Triple Watch vintage con pulsera de cuero</h5>
+                        <p class="card-text">$100.000</p>
+                        <a href="#" class="btn btn-primary">Compra aqui!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="testimonio">
+        <div class="container">
+            <h2 class="text-center">Reconocidos Compradores!</h2>
+            <div class="row">
+                <div class="col-md-4 testimonio-item">
+                    <i class="fas fa-quote-left fa-2x"></i>
+                    <p class="lead">"increibles relojes, solo los consegui en este sitio unico y especial, donde los fabrican a mano pieza por pieza cuidadosamente."</p>
+                    <h5>Joe Rogan</h5>
+                </div>
+                <div class="col-md-4 testimonial-item">
+                    <i class="fas fa-quote-left fa-2x"></i>
+                    <p class="lead">"Definitivamente con estos relojes unicos en el mundo puedo resaltar en todo tipo de reunion con personas de los altos estratos de la sociedad"</p>
+                    <h5>Andrew Tate</h5>
+                </div>
+                <div class="col-md-4 testimonial-item">
+                    <i class="fas fa-quote-left fa-2x"></i>
+                    <p class="lead">"Definitivamente seguire comprando de aqui, puro estatus"</p>
+                    <h5>Dana White</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+ </body>
+ <footer>
+  <div class="footer">
+        <div class="container">
+            <p>&copy; 2024 Aston Watches todos los derechos reservados.</p>
+
+            </footer>
+
+ </html>
+  
+
+   
